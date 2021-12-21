c12b92943a059378da418b217deaef7123744389
mmbarness <011mbarnes@gmail.com>
2021-12-20 19:10:46 -0500

    videos loading faster

--- src/features/work/workIndex.tsx
+++ src/features/work/workIndex.tsx
@@ -8,10 +8,13 @@ export const Work = () => {
 
     const dispatch = useAppDispatch();
     const allWork = useAppSelector(state => state.workSlice.work);
+    const [orientation, setOrientation] = useState(window.screen.orientation.type);
     
     useEffect(() => {
         dispatch(fetchWorkAction());
     },[dispatch])
+
+    console.log({orientation})
 
     const renderReactPlayer = (video: Video) => ([
         <ReactPlayer
@@ -39,9 +42,19 @@ export const Work = () => {
         const [desktopPlayer, mobilePlayer] = renderReactPlayer(video);
         const gridId = (i % 2 === 0) ? "left-column video-item" : "right-column video-item";
         const {credits} = video;
+        const pageOrientation = orientation.split("-").shift();
         return (<div className={gridId}>
-            {desktopPlayer}
-            {mobilePlayer}
+            {/* {desktopPlayer}
+            {mobilePlayer} */}
+            <ReactPlayer
+                url={video.link}
+                className={pageOrientation === 'landscape' ? "work-video-desktop" : "work-video-mobile"}
+                width="100%"
+                height="100%"
+                light={video.videoType === 'youtube' ? true : false}
+                controls={true}
+                origin={window.location.origin}
+            />
             <ul className="video-item-text-container">
                 <li className="video-item-text Title-li" key={`title-${video._id}`}>{video.titleToDisplay}</li>
                 {credits.map((credit) => (
@@ -49,20 +62,6 @@ export const Work = () => {
                 ))}
             </ul>
         </div>)
-    }
-    
-    const renderWork = () => {
-        let work;
-        if (allWork) {
-            work = allWork.map((video:Video, i:number) => {
-                return (
-                    <div key={video._id}>
-                        {renderLinks(video, i)}
-                    </div>
-                )
-            })
-        }
-        return (work ? work : 'loading')
     }
 
     interface Video {
@@ -85,11 +84,29 @@ export const Work = () => {
         link?: string
     }
 
+    useEffect(() => {
+        const adjustOrientation = () => {
+            setOrientation(window.screen.orientation.type)
+        }
+        window.screen.orientation.addEventListener('change', adjustOrientation)
+        if (allWork) renderWork();
+        return () => {
+            window.removeEventListener('resize', adjustOrientation)
+        }
+    },[])
+
+    const renderWork = () => (
+        allWork.map((video:Video, i:number) => 
+            <div key={video._id}>
+                {renderLinks(video, i)}
+            </div>
+    ))
+
     return(
         <div id="work-container">
             <p>VIDEO</p>
             <div id="video-container">
-                {renderWork()}
+                { (allWork) ? renderWork() : 'loading'}
             </div>
         </div>
     )
