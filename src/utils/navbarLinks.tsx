9a20784857e45e192e8340f94d94092f04c6dba2
Matthew Barnes <80414315+mmbarness@users.noreply.github.com>
2022-09-26 14:54:10 +0000

    nav links and routes share info

--- src/utils/navbarLinks.tsx
+++ src/utils/navbarLinks.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { routesObj } from "./routes";
 
 const underlineMe = (linkPath:string, pathname:string) => {
     if (linkPath === pathname){
@@ -9,6 +10,20 @@ const underlineMe = (linkPath:string, pathname:string) => {
 }
 
 export default function (pathname: string) {
+    return routesObj.filter(route => route.path !== "/").map((route, index) => {
+        return (
+            <Link 
+                key={index} 
+                to={route.path} 
+                className={underlineMe(route.path, pathname)}
+            >
+                {route.pathName}
+            </Link>
+        )
+    })
+}
+
+export function idk(pathname: string) {
     return [
         <Link to="/about" id={underlineMe("/about", pathname)} >About</Link>,
         <Link to="/reel" id={underlineMe("/reel", pathname)}>Reel</Link>,
--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -5,25 +5,32 @@ import { Work } from "../features/work/workIndex";
 import { Contact } from "../pages/contact";
 import { Home } from "../pages/home";
 
-export const Routes = () => useRoutes([
+export const routesObj = [
     {
         path: "/",
+        pathName: "Work",
         element: <Work />
     },
     { 
-        path: "/about", 
-        element: <About /> 
-    },
-    { 
-        path: "/contact", 
-        element: <Contact /> 
-    },
-    { 
         path: "/gear", 
+        pathName: "Gear",
         element: <Gear /> 
     },
     { 
         path: "/reel", 
+        pathName: "Reel",
         element: <Home /> 
     },
-])
+    { 
+        path: "/about", 
+        pathName: "About",
+        element: <About /> 
+    },
+    { 
+        path: "/contact", 
+        pathName: "Contact",
+        element: <Contact /> 
+    }
+]
+
+export const Routes = () => useRoutes(routesObj);
